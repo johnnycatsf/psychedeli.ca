c48a9b52b539dd41973f16f71f785f83b6e69cdf
Tom Scott <tubbo@psychedeli.ca>
2011-08-14 23:07:28 -0400

    MVC version of Carousel

--- js/app/models/carousel.js
+++ js/app/models/carousel.js
@@ -3,19 +3,72 @@ var Carousel = Backbone.Model.extend({
 		container: {},
 		index: 0,
 		items: new Array(),
+		currentItem: null,
 		revolving: false
 	},
 	
+	/**
+	  * Show the current item.
+	  *
+	  * @returns {DOMElement} the currently visible item
+	  */
 	show: function() {
 		var items = this.get('items');
 		var index = this.get('index');
-		
-		console.log(items);
 		
 		if (items[index].length) {
 			return items[index];
 		} else {
 			return false;
+		}
+	},
+	
+	/**
+	  * Jumps to a specific point in the array.
+	  *
+	  * @returns the new currently visible DOMElement, or `false` if it doesn't exist.
+	  */
+	go: function(newIndex) {
+		var currIndex = this.get('index');
+		var items = this.get('items');
+
+		if (typeof items[newIndex] != 'undefined') {
+			this.set({ index: newIndex });
+			return this.show();
+		} else {
+			return false;
+		}
+	},
+	
+	/**
+	  * Goes back one step.
+	  *
+	  * @returns the item previous to the current item
+	  */
+	prev: function() {
+		var currIndex = this.get('index');
+		var prevIndex = this.get('index') - 1;
+		 
+		if (this.get('revolving') == true) {
+			return (prevIndex > 0) ? this.go(prevIndex) : this.go(this.get('items').length-1);
+		} else {
+			return (prevIndex > 0) ? this.go(prevIndex) : this.show();
+		}
+	},
+	
+	/**
+	  * Goes forward one step.
+	  *
+	  * @returns the next item up from the current item
+	  */
+	next: function() {
+		var currIndex = this.get('index');
+		var nextIndex = this.get('index') + 1;
+		
+		if (this.get('revolving') == true) {
+			return (nextIndex <= this.get('items').length-1) ? this.go(nextIndex) : this.go(0);
+		} else {
+			return (nextIndex <= this.get('items').length-1) ? this.go(nextIndex) : this.show();
 		}
 	}
 });
--- js/spec/models/carousel_spec.js
+++ js/spec/models/carousel_spec.js
@@ -1,17 +1,48 @@
 describe('Carousel', function() {
-	var subjectContainer = $('<section id="posts"></section>').appendTo('body');
-	var subjectItems = new Array();
-	
-	for (var c=0; c <= 3; c++) {
-		subjectItems.push($('<article id="article'+c+'"></article>').appendTo('#posts'));
-	}
-	var subject = new Carousel({
-		container: 	subjectContainer,
-		items: 		subjectItems,
-		revolving: 	true, 
+	var subjects = (function() { var i = new Array(); for (var c=0; c <= 3; c++) { i.push($('<article id="article'+c+'"></article>')); } return i; })();
+	var carousel = new Carousel({
+		container: 	$('<section id="posts"></section>'),
+		items: 		subjects,
+		revolving: 	false
 	});
 	
 	it('should start out at position 1', function() {
-		expect(subject.show()).toBeTruthy();
+		expect(carousel.show()).toBe(subjects[0]);
+	});
+	
+	it('should move to position 3', function() {
+		expect(carousel.go(2)).toBe(subjects[2]);
+	});
+	
+	it('should move forward one position', function() {
+		expect(carousel.next()).toBe(subjects[3]);
+	});
+	
+	it('should not be able to go forward anymore', function() {
+		expect(carousel.next()).toBe(subjects[3]);
+	});
+		
+	it('should go back one position', function() {
+		expect(carousel.prev()).toBe(subjects[2]);
+	});
+	
+	it('should fail when trying to move to an off-array position', function() {
+		expect(carousel.go(9)).toBeFalsy();
+	});
+	
+	it('should cycle from the first to last position when revolving is turned on', function() {
+		carousel.set({
+			index: 0, 
+			revolving: true
+		});
+		expect(carousel.prev()).toBe(subjects[subjects.length-1]);
+	});
+	
+	it('should cycle from the last to first position when revolving is turned on', function() {
+		carousel.set({
+			index: subjects.length-1, 
+			revolving: true
+		});
+		expect(carousel.next()).toBe(subjects[0]);
 	});
 });
