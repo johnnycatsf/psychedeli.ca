22ed0c21444318dceb23571bf47db84d186530f6
Tom Scott <tubbo@psychedeli.ca>
2011-08-20 22:06:50 -0400

    restored old linkifyTags

--- js/vendor/plugins/jquery.linkifyTags.js
+++ js/vendor/plugins/jquery.linkifyTags.js
@@ -14,6 +14,53 @@
 	  * ? - Stack Overflow
 	  */
 	$.fn.linkifyTags = function(params) {
-
+		// Configuration
+		var config = {
+			defaultEngine: 'http://www.google.com/search?ie=UTF-8&q=',
+			clean: false
+		};
+		
+		// Runtime
+		return this.each(function() {
+			var self = $(this);
+			
+			var rawTags = self.html();
+			var tagArray = rawTags.split(',');
+			var tags = '';
+			
+			_.each(tagArray, function(tag) {
+				var symbol = tag.substr(0,1);
+				var tagHTML = '';
+				var cleanTag = tag.substr(1);
+				var cleanTagURL = cleanTag.split(' ').join('+');
+				tag = (config.clean) ? cleanTag : tag;
+				
+				switch(symbol) {
+					case '#': // Twitter
+						tagHTML = '<a href="http://twitter.com/#!/search/'+cleanTagURL+'" class="twitter" title="Twitter: '+cleanTagURL+'">'+tag+'</a>';
+						break;
+					case '$': // Google Finance
+						tagHTML = '<a href="http://finance.google.com/?q='+cleanTagURL+'" class="finance" title="Finance: '+cleanTagURL+'">'+tag+'</a>';
+						break;
+					case '`': // GitHub
+						tagHTML = '<a href="http://github.com/search/?q='+cleanTagURL+'" class="github" title="GitHub: '+cleanTagURL+'">'+tag+'</a>';
+						break;
+					case '~': // Facebook
+						tagHTML = '<a href="http://facebook.com/search.php?q='+cleanTagURL+'" class="facebook" title="Facebook: '+cleanTagURL+'">'+tag+'</a>';
+						break;
+					case '?': // Wikipedia 
+						tagHTML = '<a href="http://en.wikipedia.org/w/index.php?title=Special%3ASearch&search='+cleanTagURL+'" class="wikipedia" title="Wikipedia: '+cleanTagURL+'">'+tag+'</a>';
+						break;
+					case '*': // Diaspora
+					default:
+						tagHTML = '<a href="'+config.defaultEngine+tag+'" class="google" title="Google: '+tag+'">'+tag+'</a>';
+						break;
+				}
+				
+				tags += tagHTML;
+			});
+			
+			self.html(tags);
+		});
 	};
 })(jQuery)
