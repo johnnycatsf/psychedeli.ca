b19ad2f331b8249977acbaacee4b0447eeace439
Tom Scott <tubbo@psychedeli.ca>
2011-10-22 17:29:34 -0400

    everything in app.js

--- js/application.js
+++ js/application.js
@@ -1,20 +1,3 @@
-/**
-  * Main JavaScript runtime. Includes the Backbone.js MVC framework, utility libraries jQuery and 
-  * Underscore.js, and all external JavaScript plugins. Binds site-wide actions and runs browser-
-  * specific initialization code (for IE 6/7/8 and mobile browsers).
-  *
-  * @author Tom Scott
-  */
-
-// Prototypal inheritance
-if (typeof Object.create !== 'function') {
-	Object.create = function(o) {
-		function F() {}
-		F.prototype = o;
-		return new F();
-	};
-}
-
 // grab a variable from GET params
 function GET(name) {
 	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
@@ -29,24 +12,10 @@ function GET(name) {
 	}
 }
 
-var dependencies = [
-						'jquery',
-						'lib/jquery-ui',
-						'lib/plugins/pretty'
-					];
-require(dependencies, function($) {
-	// // Page title helper
-	// var pageTitle = $('title').html();
-	// if (pageTitle != 'psychedeli.ca') {
-	// 	$('title').html(pageTitle+' | psychedeli.ca');
-	// }
-
-	// Tags helper
-	//$('.tags').linkifyTags();
-
-	// Modal dialog helper (comments)
+// DOM ready events
+$(document).ready(function() {
 	$('a[rel=modal]').facebox({
 		closeImage: 'img/closelabel.png',
 		loadingImage: 'img/loading.gif'
 	});
-});
+})
