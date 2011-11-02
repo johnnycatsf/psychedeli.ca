03d676e0dacfe2205142efecea5691395534bac3
Tom Scott <tubbo@psychedeli.ca>
2011-11-01 21:29:30 -0400

    made some changes to jquery.ticker, added objects

--- js/jquery.ticker.js
+++ js/jquery.ticker.js
@@ -1,109 +1,87 @@
-"use strict";
+/**
+ * jQuery.ticker is a plugin that constantly polls various social networks and displays each status message or notification in a ticker-style format.
+ *
+ * Supported services: Facebook, Twitter, GitHub, SoundCloud
+ *
+ * @author Tom Scott
+ */
 
-(function($) {
-	var Ticker = function(self, config) {
-		var currentStatus, auth, feed, feedIndex;
+"use strict";
+(function($) {	
+	$.fn.ticker = function(params) {
+		// Blip is the interface for all services to enter the ticker. It is a standard means of accessing status messages.
+		var Blip = function(setup) {
+			this.id			= (setup.id)		? setup.id			: 0;
+			this.service 	= (setup.service) 	? setup.service 	: '';
+			this.message 	= (setup.message) 	? setup.message 	: '';
+			this.location 	= (setup.location)	? setup.location 	: '';
+		};
 		
-		this.init = function() {
-			// instantiate current status message
-			currentStatus = {
-				provider: 'none',
-				message: ''
+		// Ticker is a collection of Blips.
+		var Ticker = function() {
+			var blips = new Array();
+			
+			this.length = function() {
+				return blips.length;
 			};
 			
-			// instantiate authentication details
-			auth.accessToken = config.access;
-			auth.secretToken = config.secret;
-			auth.accountName = config.handle;
+			this.add = function(newBlip) {
+				var blipNotFound = true;
+				
+				// make sure this blip isn't already in
+				var c = 0;
+				while (blipNotFound && c < blips.length) {
+					blipNotFound = (blips[c].id == newBlip.id) ? false : blipNotFound;
+					c++;
+				}
+				
+				// if not found, push to the end of the array
+				return (blipNotFound) ? blips.push(newBlip) : false;
+			};
 			
-			// instantiate feed and feed index
-			feed = [];
-			feedIndex = 0;
-			
-			// cache the jQuery DOM object in memory
-			this.element = self;
-			
-			// pull the whole feed into var feed
-			
-			// sort var feed
-			feed.sort(function(a,b) {
-				return a.date - b.date;
-			});
+			this.pop = function() {
+				return blips.pop();
+			};
 		};
 		
-		/**
-		  * Updates the #ticker element with the next status in the feed.
-		  */
-		this.update = function() {
-			this.element.find('ul').append('<li><img src="ticker/'+currentStatus.provider+'.png" alt="'+currentStatus.provider+'"> '+currentStatus.message+'</li>');
-		};
-		
-		/**
-		  * Returns the current status, or `false` if there are none.
-		  */
-		this.status = function() {
-			if (currentStatus.message != '') {
-				return currentStatus;
-			} else {
-				return false;
-			}
-		};
-		
-		/**
-		  * Goes to a specific index in the feed. This is an absolute array index, so +1 to find the actual position of the ticker message.
-		  */
-		this.to = function(index) {
-			feedIndex = index;
-			currentStatus = feed[index];
-			return this.status();
-		}
-		
-		/**
-		  * Iterates to the next status in the feed, and saves it in memory. Also runs this.status() to return the current status.
-		  */
-		this.next = function() {
-			return this.to(feedIndex++);
-		};
-		
-		/**
-		  * Backtracks to the previous status in the feed, and saves it in memory. Also runs this.status() to return the current status.
-		  */
-		this.prev = function() {
-			return this.to(feedIndex--);
-		};
-		
-		
-		this.init();
-	};
-	
-	$.fn.ticker = function(params) {
+		// Plugin configuration
 		var config = {
+			pollEvery: 5000,
+			changeEvery: 5000
 			twitter: {
 				handle: ''
 				access: ''
 				secret: ''
 			},
 			facebook: {
-				access: ''
-				secret: ''
+				app: {
+					id: '',
+					secret: ''
+				},
+				usr: {
+					token: ''
+				}
 			},
 			github: {
-				access: '',
-				secret: ''
+				api_key: '',
 			},
 			soundcloud: {
 				access: '',
 				secret: ''
 			}
 		};
-		
 		$.fn.extend(config, params);
 		
-		var ticker = new Ticker(this, config.twitter);
+		// Poll the API and transition Blips
+		var polling = setInterval(function() {
+			// Poll Facebook and add all of its 
+			$.ajax({
+				
+			})
+		}, config.pollEvery);
 		
-		// poll for changes
-		var interval = setInterval(function() {
+		var changing = setInterval(function() {
 			
-		}, 5000);
+		}, config.changeEvery)
 	};
 })(jQuery)
