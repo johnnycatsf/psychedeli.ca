693c5ff0f2390500e04485c2cfd1481e91f54567
Tom Scott <tubbo@psychedeli.ca>
2011-12-03 23:11:24 -0500

    wow new styles

--- config.ru
+++ config.ru
@@ -3,7 +3,7 @@
 #
 require 'rack'
 require 'rack/contrib/try_static'
-require 'lib/status_exchange'
+#require './lib/status_exchange'
 
 #
 # frontend
@@ -12,6 +12,6 @@ use Rack::TryStatic,
   :root => 'pub',
   :urls => %w[/],
   :try => ['.html', 'index.html', '/index.html']
-use StatusExchange
+#use StatusExchange
 # otherwise 404
 run lambda { [404, {'Content-Type' => 'text/html'}, ['404: Request Not Found']]}
--- lib/status_exchange.rb
+++ lib/status_exchange.rb
@@ -18,21 +18,20 @@ require 'mogli'
 
 CONFIG = YAML::load_file('_config.yml')
 
-Twitter.configure do |config|
-  config.consumer_key = CONFIG['twitter']['app']['token']
-  config.consumer_secret = CONFIG['twitter']['app']['secret']
-  config.oauth_token = CONFIG['twitter']['usr']['token']
-  config.oauth_token_secret = CONFIG['twitter']['usr']['secret']
-end
+twitter = Twitter.new
+
+# Twitter.configure do |config|
+#   config.consumer_key = CONFIG['twitter']['app']['token']
+#   config.consumer_secret = CONFIG['twitter']['app']['secret']
+#   config.oauth_token = CONFIG['twitter']['usr']['token']
+#   config.oauth_token_secret = CONFIG['twitter']['usr']['secret']
+# end
 
 class StatusExchange < Sinatra::Base
   get '/status.json' do
     facebook_client = Mogli::Client.new(CONFIG['facebook']['access_token'])
+
     facebook = Mogli::User.find('me', facebook_client)
-    
-    twitter = Twitter.home_timeline
-    
-    github = GitHub.create
-    
+    twitter = Twitter.user_timeline('tubbo')
   end
-    
+end
