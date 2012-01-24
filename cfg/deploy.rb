c6ccbb9863056a85c914bf972bda2d86840890ae
Tom Scott <tubbo@psychedeli.ca>
2012-01-24 00:08:31 -0500

    removed all submodules to fix deployer

--- cfg/deploy.rb
+++ cfg/deploy.rb
@@ -1,23 +1,47 @@
-set :application, "set your application name here"
-set :repository,  "git@psychedeli.ca:code/blog"
+# = Deployment Configuration
+#
+# Deployment is handled with Capistrano. Via the Capfile, this configuration is
+# called when <tt>cap deploy</tt> is run from the shell.
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# == Authentication
+#
+# When deployments happen, always log in as <tt>necromancer@psychedeli.ca</tt>
+# (make sure the proper key is set up) and set <tt>psychedeli.ca</tt> to be the
+# domain for all 3 roles.
+set :user, "necromancer"
+set :domain, "psychedeli.ca"
+server domain, :app, :web
+role :db, domain, :primary => true
 
-role :web, "psychedeli.ca"                          # Your HTTP server, Apache/etc
-role :app, "psychedeli.ca"                          # This may be the same as your `Web` server
-role :db,  "psychedeli.ca", :primary => true # This is where Rails migrations will run
+# == Application
+#
+# Where to put the application once it reaches the server, and what it's called
+set :application, "blog"
+set :deploy_to, "/home/#{user}/src/#{application}"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# == Version control
+#
+# Uses a private git server for source control, hosted on psychedeli.ca. This
+# section also configures a number of SSH options since Git relies so heavily
+# on SSH for communication.
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :repository, "git@psychedeli.ca:code/blog.git"
+set :scm, "git"
+set :user, "necromancer"
+set :use_sudo, true
+set :git_enable_submodules, 1
 
-# If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-  task :start do
-  end
-  task :stop do
-  end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+# == Passenger
+#
+# psychedeli.ca is powered by Passenger. Here's a deploy task that makes#
+# restarting a bit easier.
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
+
+# Always restart the app after deployment
+after :deploy, "passenger:restart"
