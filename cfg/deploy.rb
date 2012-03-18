235f85d6b31eac46d865fa4c884967f063bbeb4e
Tom Scott <tubbo@psychedeli.ca>
2012-03-18 17:30:40 -0400

    updated rvmrc and deploy script

--- cfg/deploy.rb
+++ cfg/deploy.rb
@@ -15,5 +15,16 @@ set :application, "blog"
 set :deploy_to, "/home/#{user}/src/#{application}"
 role :web, "psychedeli.ca"
 
+namespace :deploy do
+  desc "Restart the server and recompile the app"
+  task :update_content do
+    system "cd #{current_release}; rm -rf pub/*"
+    system "cd #{current_release}; bundle exec jekyll --config=cfg/jekyll.yml"
+  end
+  task :restart do
+    system "cd #{current_release}; touch tmp/restart.txt"
+  end
+end
+
 # Always restart the app after deployment
 after 'deploy:update_code', 'deploy:update_content'
