c9889068b8f26cbad16fb578d76cddc360ace8a8
Tom Scott <tubbo@psychedeli.ca>
2012-03-27 00:07:53 -0400

    made vcr record only new episodes and regenerated cassettes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,6 +16,7 @@ require 'rack/test'
 require 'capybara'
 
 VCR.configure do |c|
+  c.default_cassette_options = { :record => :new_episodes }
   c.cassette_library_dir = File.expand_path './test/cassettes/'
   c.hook_into :webmock
 end
