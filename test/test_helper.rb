6b14751c1fbad67af1162394cf1810b3090af9bf
Tom Scott <tubbo@psychedeli.ca>
2012-04-01 16:27:58 -0400

    don't record new episodes for travis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,7 @@ require 'rack/test'
 require 'capybara'
 
 VCR.configure do |c|
-  c.default_cassette_options = { :record => :new_episodes }
+  # c.default_cassette_options = { :record => :new_episodes }
   c.cassette_library_dir = File.expand_path './test/cassettes/'
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = true
