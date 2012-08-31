85b1be41204ac932c0ec3290374ac83202afa9df
Tom Scott <tubbo@psychedeli.ca>
2012-08-31 16:07:54 -0400

    Try a constant

--- config/initializers/status_exchange.rb
+++ config/initializers/status_exchange.rb
@@ -1,28 +1,26 @@
 module Psychedelica
   class Application
+    ENV_CONFIG = {
+      twitter: { user_name: ENV['TWITTER_USER_NAME'] },
+      facebook: {
+        app_id: ENV['FACEBOOK_APP_ID'],
+        app_secret: ENV['FACEBOOK_APP_SECRET'],
+        access_token: ENV['FACEBOOK_ACCESS_TOKEN'],
+        vanity_url: ENV['FACEBOOK_VANITY_URL']
+      },
+      github: { user_name: ENV['GITHUB_USER_NAME'] }
+    }
+
     config.status_providers = if Rails.env == 'stage'
-      environment_defines_config!
+      ENV_CONFIG
     else
       status_config_path = "#{Rails.root}/config/status_exchange.yml"
 
       if File.exists? status_config_path
         HashWithIndifferentAccess.new YAML::load_file(status_config_path)[Rails.env]
       else
-        environment_defines_config!
+        ENV_CONFIG
       end
-    end
-
-    def environment_defines_config!
-      {
-        twitter: { user_name: ENV['TWITTER_USER_NAME'] },
-        facebook: {
-          app_id: ENV['FACEBOOK_APP_ID'],
-          app_secret: ENV['FACEBOOK_APP_SECRET'],
-          access_token: ENV['FACEBOOK_ACCESS_TOKEN'],
-          vanity_url: ENV['FACEBOOK_VANITY_URL']
-        },
-        github: { user_name: ENV['GITHUB_USER_NAME'] }
-      }
     end
   end
 end
