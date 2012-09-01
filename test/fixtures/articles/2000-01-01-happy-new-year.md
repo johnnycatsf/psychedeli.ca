bfec8061c27b3bf6c9ae2f84c4aff0f84a15a8b6
Tom Scott <tubbo@psychedeli.ca>
2012-08-31 20:21:14 -0400

    Add test article source

---
+++ test/fixtures/articles/2000-01-01-happy-new-year.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "happy new year!"
+date: 2000-01-01
+tags: "#newyears, #y2k"
+---
+
+It's gonna be a wild party.
--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup { @article = Article.find "2000-01-01-happy-new-year" }
+
+  test "find an article by its id" do
+    assert @article.present?, "Article was not found"
+  end
+
+  test "read the article's yaml front matter as a hash" do
+    assert_equal "happy new year", @article.title
+  end
+
+  test "read the article without its yaml front matter" do
+    assert_equal "happy new year!", @article.content.split("\n").first
+  end
 end
