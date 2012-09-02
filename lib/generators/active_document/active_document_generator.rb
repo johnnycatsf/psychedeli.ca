eac118da3223ef6275d35c4dff448d50e84b1f07
Tom Scott <tubbo@psychedeli.ca>
2012-09-02 01:47:16 -0400

    Fix generator

--- lib/generators/active_document/active_document_generator.rb
+++ lib/generators/active_document/active_document_generator.rb
@@ -2,13 +2,14 @@ class ActiveDocumentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def generate_ruby
-    copy_template "model.rb", "app/models/#{file_name}.rb"
-    copy_template "test.rb", "test/unit/#{file_name}_test.rb"
+    copy_file "model.rb", "app/models/#{file_name}.rb"
+    copy_file "test.rb", "test/unit/#{file_name}_test.rb"
   end
 
   def generate_markdown
     %w(app/documents test/fixtures/documents).each do |root_path|
-      copy_template "example.md", "#{root_path}/article.md"
+      file_path = "#{root_path}/#{plural_table_name}/example.md"
+      copy_file "example.md", file_path
     end
   end
 end
