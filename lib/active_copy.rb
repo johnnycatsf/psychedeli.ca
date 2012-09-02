ecb5f989ee2b4549835547c2c70b38492737c245
Tom Scott <tubbo@psychedeli.ca>
2012-09-02 03:09:53 -0400

    Fix runtime bugs

--- lib/active_copy.rb
+++ lib/active_copy.rb
@@ -1,6 +1,12 @@
 require 'bundler'
 Bundler.require :active_copy
 
+require 'active_copy/base'
+require 'active_copy/attributes'
+require 'active_copy/compiler'
+require 'active_copy/markdown'
+require 'active_copy/tasks_helper'
+
 # ActiveCopy reads Markdown files in +app/documents+ instead of a
 # database for your Rails models. Inspired by Jekyll, it uses YAML front
 # matter to set up metadata for each page, then renders it using
@@ -8,4 +14,11 @@ Bundler.require :active_copy
 # the Markdown files to pure HTML for performance purposes.
 module ActiveCopy
   VERSION = 'alpha1'
+
+  # Find all models in the Rails app that inherit from ActiveCopy::Base.
+  def self.models
+    Dir["#{Rails.root}/app/documents/*/"].map do |folder| 
+      File.basename(folder).singularize.camelize.constantize
+    end
+  end
 end
