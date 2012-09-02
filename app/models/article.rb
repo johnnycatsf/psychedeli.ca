eb1091b573d6e5f954e6d2fd6519705bd1ed4855
Tom Scott <tubbo@psychedeli.ca>
2012-09-02 02:22:56 -0400

    Rename existing articles so shit don't break

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 # Represents a single article in the blog.
-class Article < ActiveDocument::Base
+class Article < ActiveCopy::Base
   attr_accessible :layout, :title, :category, :date, :tags, :hn_item_id, :published
 
   deploy_to "#{public_path}/#{date.year}/#{date.month}/#{date.day}/index.html"
