5777609c4a055763afa81b50c317ea110ef81e63
Tom Scott <tubbo@psychedeli.ca>
2012-11-30 19:42:07 -0500

    Perform caching on Heroku

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -7,8 +7,7 @@ Psychedelica::Application.configure do
   # Full error reports are disabled
   config.consider_all_requests_local       = false
 
-  # Don't cache on Heroku
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
