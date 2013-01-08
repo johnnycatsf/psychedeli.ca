8fa7761206bc68b5039c619f58629466339b312e
Tom Scott <tubbo@psychedeli.ca>
2013-01-08 09:33:55 -0500

    Add article generator, remove old code
    - Remove old Jekyll code
    - Remove generic document generator
    - Add domain-specific Article generator

---
+++ lib/generators/article/article_generator.rb
@@ -0,0 +1,33 @@
+# Generate an Article with ActiveCopy.
+class ArticleGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+
+  class_option :category, aliases: '-c',
+                         desc: 'Category that this article is filed under',
+                      default: 'gbs'
+
+  class_option :tags,     aliases: '-t',
+                         desc: 'Tags to organize this article with.',
+                      default: ''
+
+  def do_it_to_it
+    copy_file "article.md", "#{Rails.root}/app/views/articles/content/#{id}.md"
+  end
+
+private
+  def id
+    @article_id ||= "#{date}-#{file_name}"
+  end
+
+  def date
+    @date_string ||= Time.now.strftime '%Y-%m-%d'
+  end
+
+  def title
+    @title ||= file_name.titleize
+  end
+
+  def file_name
+    human_name.parameterize
+  end
+end
