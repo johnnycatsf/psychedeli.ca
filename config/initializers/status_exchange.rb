8c708cab9a4b993b7d7343b8cd454068a3b1429e
Tom Scott <tubbo@psychedeli.ca>
2013-02-10 20:14:17 -0500

    Fix shell var names

--- config/initializers/status_exchange.rb
+++ config/initializers/status_exchange.rb
@@ -1,31 +1,22 @@
-module Psychedelica
-  class Application
-    ENV_CONFIG = {
-      twitter: { user_name: ENV['TWITTER_USER_NAME'] },
-      facebook: {
-        app_id: ENV['FACEBOOK_APP_ID'],
-        app_secret: ENV['FACEBOOK_APP_SECRET'],
-        access_token: ENV['FACEBOOK_ACCESS_TOKEN'],
-        vanity_url: ENV['FACEBOOK_VANITY_URL']
-      },
-      github: { user_name: ENV['GITHUB_USER_NAME'] }
-    }
-
-    config.status_providers = if Rails.env == 'stage'
-      ENV_CONFIG
-    else
-      status_config_path = "#{Rails.root}/config/status_exchange.yml"
-
-      if File.exists? status_config_path
-        HashWithIndifferentAccess.new YAML::load_file(status_config_path)[Rails.env]
-      else
-        ENV_CONFIG
-      end
-    end
-  end
-end
+Psychedelica::Application.config.status_providers = {
+  twitter: {
+    user_name: ENV['TWITTER_USER_NAME'],
+    consumer_key: ENV['TWITTER_CONSUMER_KEY'],
+    consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
+    oauth_token: ENV['TWITTER_ACCESS_KEY'],
+    oauth_token_secret: ENV['TWITTER_ACCESS_SECRET']
+  },
+  facebook: {
+    app_id: ENV['FACEBOOK_APP_ID'],
+    app_secret: ENV['FACEBOOK_APP_SECRET'],
+    access_token: ENV['FACEBOOK_ACCESS_TOKEN'],
+    vanity_url: ENV['FACEBOOK_VANITY_URL']
+  },
+  github: { user_name: ENV['GITHUB_USER_NAME'] }
+}
 
 StatusConfig = Psychedelica::Application.config.status_providers
+
 Twitter.configure do |config|
   config.consumer_key = StatusConfig[:twitter][:consumer_key]
   config.consumer_secret = StatusConfig[:twitter][:consumer_secret]
