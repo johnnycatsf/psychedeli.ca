4e3ab824f070998ae536dbea4b5697dd5d8a2301
Tom Scott <tubbo@psychedeli.ca>
2013-04-02 22:13:47 -0400

    Fix meta tags not displaying

--- app/helpers/markup_helper.rb
+++ app/helpers/markup_helper.rb
@@ -3,20 +3,18 @@ module MarkupHelper
   # ones defined in the YAML, then it renders the CSRF meta tags
   # provided by Rails.
   def meta_tags
-    content_tag :meta, charset: "utf-8"
-    content_tag :meta, "http-equiv" => "X-UA-Compatible", :content => "IE=edge,chrome=1"
-    content_tag :meta, name: "viewport", content: "width=device-width"
-
-    Psychedelica::Application.config.metadata.each do |type,body|
-      content_tag :meta, name: type, content: body
-    end
-
-    csrf_meta_tags
+    render 'meta_tags'
   end
 
   # A helper method for prefetching static content, such as images
   # or other media, that we use often.
   def prefetch_content at_location
     tag :link, rel: 'prefetch', href: at_location
+  end
+
+  def custom_meta_tags
+    Psychedelica::Application.config.metadata.reduce("") { |tags,(type,body)|
+      tags += "#{tag :meta, name: type, content: body.html_safe}\n"
+    }.html_safe
   end
 end
